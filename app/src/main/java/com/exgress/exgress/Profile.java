723234279809750a91440d6ca1f17a04f4a22414
mahmoodhegazy <mahmoudhegazya@gmail.com>
2015-11-06 23:57:55 -0500

    uptdate 1.0

--- app/src/main/java/com/exgress/exgress/Profile.java
+++ app/src/main/java/com/exgress/exgress/Profile.java
@@ -12,10 +12,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.microsoft.band.BandClient;
+import com.microsoft.band.BandClientManager;
+import com.microsoft.band.BandException;
+import com.microsoft.band.BandInfo;
+import com.microsoft.band.BandIOException;
+import com.microsoft.band.BandPendingResult;
+import com.microsoft.band.ConnectionState;
+import com.microsoft.band.sensors.BandHeartRateEvent;
+import com.microsoft.band.sensors.BandHeartRateEventListener;
+import com.microsoft.band.sensors.HeartRateConsentListener;
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+
 
 public class Profile extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+
+    private BandClient bandClient;
+    private TextView txtStatus;
+    private Button btnStart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +61,77 @@ public class Profile extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        // initialize bands
+        BandInfo[] pairedBands = BandClientManager.getInstance().getPairedBands();
+
+        this.bandClient = BandClientManager.getInstance().create(Profile.this.getApplicationContext(), pairedBands[0]);
+
+        BandPendingResult<ConnectionState> pendingResult =
+                bandClient.connect();
+        try {
+            ConnectionState state = pendingResult.await();
+            if(state == ConnectionState.CONNECTED) {
+// do work on success
+                System.out.println("Connection Success");
+            } else {
+// do work on failure
+                System.out.println("Connection Failed :( , Try Again !");
+            }
+        } catch(InterruptedException ex) {
+// handle InterruptedException
+        } catch(BandException ex) {
+// handle BandException
+        }
+
+        String hwVersion = "19";
+        String fwVersion = "10.3.3304.0";       // should be this or later
+        try {
+            fwVersion = bandClient.getFirmwareVersion().await();
+            hwVersion = bandClient.getHardwareVersion().await();
+        } catch (InterruptedException ex) {
+// handle InterruptedException
+        } catch (BandIOException ex) {
+// handle BandIOException
+        } catch (BandException ex) {
+// handle BandException
+        }
+    }
+
+     private HeartRateConsentListener mHeartRateConsentListener = new HeartRateConsentListener();
+    public void startHRListener() {
+        try {
+            // register HR sensor event listener
+            bandClient.getSensorManager().registerHeartRateEventListener(mHeartRateConsentListener);
+        } catch (BandIOException ex) {
+            appendToUI(ex.getMessage(), band);
+        } catch (BandException e) {
+            String exceptionMessage="";
+            switch (e.getErrorType()) {
+                case UNSUPPORTED_SDK_VERSION_ERROR:
+                    exceptionMessage = "Microsoft Health BandService doesn't support your SDK Version. Please update to latest SDK.";
+                    break;
+                case SERVICE_ERROR:
+                    exceptionMessage = "Microsoft Health BandService is not available. Please make sure Microsoft Health is installed and that you have the correct permissions.";
+                    break;
+                default:
+                    exceptionMessage = "Unknown error occurred: " + e.getMessage();
+                    break;
+            }
+            appendToUI(exceptionMessage, band);
+
+        } catch (Exception e) {
+            appendToUI(e.getMessage(), band);
+        }
+    }
+    private void appendToUI(final String string) {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                txtStatus.setText(string);
+            }
+        });
     }
 
     @Override
