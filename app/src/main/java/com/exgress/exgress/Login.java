994ec529185922077b7c5f3153ea5d09fe15f022
Ronak <ronakp991@gmail.com>
2015-11-07 11:05:12 -0500

    HTTP Client

--- app/src/main/java/com/exgress/exgress/Login.java
+++ app/src/main/java/com/exgress/exgress/Login.java
@@ -1,14 +1,19 @@
 package com.exgress.exgress;
 
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Login extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +22,18 @@ public class Login extends AppCompatActivity {
     }
 
     public void loginpro(View v) {
+        InputStream in = null;
+        try {
+            URL url = new URL("");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            in = new BufferedInputStream(urlConnection.getInputStream());
+        } catch (Exception e ) {
+            System.out.println(e.getMessage());
+        }
         Intent intent1 = new Intent(this, Profile.class);
         startActivity(intent1);
     }
+
     public void registerpro(View v) {
         Intent intent2 = new Intent(this, Register.class);
         startActivity(intent2);
