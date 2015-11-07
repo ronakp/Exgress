94b4cd6c18815049a3e46309a7e8385847a6b626
mahmoodhegazy <mahmoudhegazya@gmail.com>
2015-11-07 10:08:21 -0500

    Location debugged

--- app/src/main/java/com/exgress/exgress/GetCurrentLocation.java
+++ app/src/main/java/com/exgress/exgress/GetCurrentLocation.java
@@ -1,9 +1,5 @@
 package com.exgress.exgress;
 
-/**
- * Created by mahmoodhegazy on 15-11-07.
- */
-
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
@@ -34,10 +30,10 @@ import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.os.Build;
 import android.content.Context;
-import com.google.android.gms.location.LocationServices;
-
-public class GetCurrentLocation extends Activity
-        implements OnClickListener {
+/**
+ * Created by mahmoodhegazy on 15-11-07.
+ */
+public class GetCurrentLocation extends Activity implements OnClickListener{
 
     private LocationManager locationMangaer=null;
     private LocationListener locationListener=null;
@@ -63,7 +59,7 @@ public class GetCurrentLocation extends Activity
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.location);
 
 
         //if you want to lock screen for always Portrait mode
@@ -94,7 +90,7 @@ public class GetCurrentLocation extends Activity
                     " see the changes in coordinates." + "\nWait..");
 
             pb.setVisibility(View.VISIBLE);
-            locationListener = new Loaction();
+            locationListener = new MyLocationListener();
 
             if ( Build.VERSION.SDK_INT >= 23 &&
                     ContextCompat.checkSelfPermission( context, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED &&
@@ -140,7 +136,7 @@ public class GetCurrentLocation extends Activity
                     }
                 }
             } catch (Exception ex)  {
-              //  LogService.log( "Error creating location service: " + ex.getMessage() );
+                //  LogService.log( "Error creating location service: " + ex.getMessage() );
 
             }
 
@@ -195,58 +191,56 @@ public class GetCurrentLocation extends Activity
         AlertDialog alert = builder.create();
         alert.show();
     }
-    public class Loaction implements LocationListener{
+    public class MyLocationListener implements LocationListener{
 
-    @Override
-    public void onLocationChanged(Location loc) {
+        @Override
+        public void onLocationChanged(Location loc) {
 
-        editLocation.setText("");
-        pb.setVisibility(View.INVISIBLE);
-        Toast.makeText(getBaseContext(),"Location changed : Lat: " +
-                        loc.getLatitude()+ " Lng: " + loc.getLongitude(),
-                Toast.LENGTH_SHORT).show();
-        String longitude = "Longitude: " +loc.getLongitude();
-        Log.v(TAG, longitude);
-        String latitude = "Latitude: " +loc.getLatitude();
-        Log.v(TAG, latitude);
+            editLocation.setText("");
+            pb.setVisibility(View.INVISIBLE);
+            Toast.makeText(getBaseContext(),"Location changed : Lat: " +
+                            loc.getLatitude()+ " Lng: " + loc.getLongitude(),
+                    Toast.LENGTH_SHORT).show();
+            String longitude = "Longitude: " +loc.getLongitude();
+            Log.v(TAG, longitude);
+            String latitude = "Latitude: " +loc.getLatitude();
+            Log.v(TAG, latitude);
 
     /*----------to get City-Name from coordinates ------------- */
-        String cityName=null;
-        Geocoder gcd = new Geocoder(getBaseContext(),
-                Locale.getDefault());
-        List<Address>  addresses;
-        try {
-            addresses = gcd.getFromLocation(loc.getLatitude(), loc
-                    .getLongitude(), 1);
-            if (addresses.size() > 0)
-                System.out.println(addresses.get(0).getLocality());
-            cityName=addresses.get(0).getLocality();
-        } catch (IOException e) {
-            e.printStackTrace();
+            String cityName=null;
+            Geocoder gcd = new Geocoder(getBaseContext(),
+                    Locale.getDefault());
+            List<Address>  addresses;
+            try {
+                addresses = gcd.getFromLocation(loc.getLatitude(), loc
+                        .getLongitude(), 1);
+                if (addresses.size() > 0)
+                    System.out.println(addresses.get(0).getLocality());
+                cityName=addresses.get(0).getLocality();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            String s = longitude+"\n"+latitude +
+                    "\n\nMy Currrent City is: "+cityName;
+            editLocation.setText(s);
         }
 
-        String s = longitude+"\n"+latitude +
-                "\n\nMy Currrent City is: "+cityName;
-        editLocation.setText(s);
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-        // TODO Auto-generated method stub
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-        // TODO Auto-generated method stub
-    }
-
-    @Override
-    public void onStatusChanged(String provider,
-                                int status, Bundle extras) {
-        // TODO Auto-generated method stub
-    }
-}
-}
-
-
+        @Override
+        public void onProviderDisabled(String provider) {
+            // TODO Auto-generated method stub
         }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onStatusChanged(String provider,
+                                    int status, Bundle extras) {
+            // TODO Auto-generated method stub
+        }
+    }
+}
+
