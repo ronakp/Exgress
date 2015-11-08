dc814f8115f01338ff36d15772f16fb3681855b5
mahmoodhegazy <mahmoudhegazya@gmail.com>
2015-11-07 23:53:41 -0500

    no message

--- app/src/main/java/com/exgress/exgress/World.java
+++ app/src/main/java/com/exgress/exgress/World.java
@@ -19,7 +19,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class World extends AppCompatActivity implements OnMapReadyCallback {
+public class World extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private MapView mapView;
@@ -39,48 +39,48 @@ public class World extends AppCompatActivity implements OnMapReadyCallback {
     }
 
 
-    /*@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.world, container, false);
-
-        // Gets the MapView from the XML layout and creates it
-        mapView = (MapView) v.findViewById(R.id.map);
-        mapView.onCreate(savedInstanceState);
-
-        // Gets to GoogleMap from the MapView and does initialization stuff
-        mMap = mapView.getMap();
-        mMap.getUiSettings().setMyLocationButtonEnabled(false);
-        mMap.setMyLocationEnabled(true);
-
-        // Needs to call MapsInitializer before doing any CameraUpdateFactory calls
-        MapsInitializer.initialize(this.getApplicationContext());
-
-
-        // Updates the location and zoom of the MapView
-        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(43.1, -87.9), 10);
-        mMap.animateCamera(cameraUpdate);
-
-        return v;
-    }*/
-
-    @Override
-    public void onResume() {
-        mapView.onResume();
-        super.onResume();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        mapView.onDestroy();
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapView.onLowMemory();
-    }
-
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        View v = inflater.inflate(R.layout.world, container, false);
+//
+//        // Gets the MapView from the XML layout and creates it
+//        mapView = (MapView) v.findViewById(R.id.map);
+//        mapView.onCreate(savedInstanceState);
+//
+//        // Gets to GoogleMap from the MapView and does initialization stuff
+//        mMap = mapView.getMap();
+//        mMap.getUiSettings().setMyLocationButtonEnabled(false);
+//        mMap.setMyLocationEnabled(true);
+//
+//        // Needs to call MapsInitializer before doing any CameraUpdateFactory calls
+//        MapsInitializer.initialize(this.getActivity());
+//
+//
+//        // Updates the location and zoom of the MapView
+//        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(43.1, -87.9), 10);
+//        mMap.animateCamera(cameraUpdate);
+//
+//        return v;
+//    }
+//
+//    @Override
+//    public void onResume() {
+//        mapView.onResume();
+//        super.onResume();
+//    }
+//
+//    @Override
+//    public void onDestroy() {
+//        super.onDestroy();
+//        mapView.onDestroy();
+//    }
+//
+//    @Override
+//    public void onLowMemory() {
+//        super.onLowMemory();
+//        mapView.onLowMemory();
+//    }
+//
 
 
     /**
